d21e44b4e10625b3e317f48f495004114709efe9
Sahil Sharma <49686087+sahilllSharma@users.noreply.github.com>
2020-08-11 17:19:44 +0530

    Add files via upload

---
+++ test/java/com/windsor/web/VirtualAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windsor.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
